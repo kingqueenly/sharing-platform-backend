520c9d675ee643ac7a099ac745afa14a023564a8
kui.li <kui.li@accenture.com>
2017-06-14 15:40:25 +0800

    Sharing Platform backend repository init

--- src/main/java/com/sharingplatform/SharingPlatformApplication.java
+++ src/main/java/com/sharingplatform/SharingPlatformApplication.java
@@ -10,9 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
 //extends SpringBootServletInitializer
-public class BenzAcApplication  extends SpringBootServletInitializer{
+public class SharingPlatformApplication  extends SpringBootServletInitializer{
     public static void main(String[] args) {
-        SpringApplication.run(BenzAcApplication.class, args);
+        SpringApplication.run(SharingPlatformApplication.class, args);
     }
 
     @Override
